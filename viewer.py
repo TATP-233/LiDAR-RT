6212196532eb68bf11327aa8ed3a194a957fe131
jyf23 <1305116641@qq.com>
2025-06-15 20:59:51 +0800

    update render

--- viewer.py
+++ viewer.py
@@ -10,8 +10,22 @@ from lib.utils.image_utils import color_mapping
 
 def vis_pcd(pcd_path, point_size=3.0, colormap=matplotlib.colormaps["rainbow"]):
     pcd = o3d.io.read_point_cloud(pcd_path)
+    
+    # 将点云数据转换为PyTorch张量
+    points = torch.from_numpy(np.asarray(pcd.points))
+    points = points[torch.norm(points, dim=1) > 1e-6]
+    
+    # 计算点云重心
+    centroid = torch.mean(points, dim=0)
+    
+    # 将点云移动到重心位置
+    points = points - centroid
+    
+    # 更新点云数据
+    pcd.points = o3d.utility.Vector3dVector(points.numpy())
+    points = torch.from_numpy(np.asarray(pcd.points))
 
-    z = torch.from_numpy(np.asarray(pcd.points)[:, 2])
+    z = points[:, 2]
     max_q = torch.quantile(z, 0.9)
     min_q = torch.quantile(z, 0.1)
     z = (z - min_q) / (max_q - min_q)
@@ -23,6 +37,13 @@ def vis_pcd(pcd_path, point_size=3.0, colormap=matplotlib.colormaps["rainbow"]):
     vis = o3d.visualization.Visualizer()
     vis.create_window()
     vis.add_geometry(pcd)
+    
+    # 创建并添加坐标系
+    coordinate_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(
+        size=1.0,  # 坐标系大小
+        origin=[0, 0, 0]  # 坐标系原点位置
+    )
+    vis.add_geometry(coordinate_frame)
 
     render_option = vis.get_render_option()
     render_option.point_size = point_size
